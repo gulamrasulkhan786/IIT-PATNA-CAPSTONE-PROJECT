eb0ba9858e494c07e6b17155c6cb9db39dea6376
gulamrasulkhan786 <gulam_ua2504cdh74@iitp.ac.in>
2026-04-28 16:31:51 +0530

    Update api.js

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,1 +1,11 @@
+import axios from "axios";
 
+const api = axios.create({
+  const BASE_URL =
+  process.env.REACT_APP_API_URL || "https://awarness-data-anylasis.onrender.com";
+
+const api = axios.create({
+  baseURL: BASE_URL,
+});
+
+export default api;
